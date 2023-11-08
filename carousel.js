5372266924b3fd7cebbbd4684c1f96b4a9634d24
adribj12 <adricap1234@gmail.com>
2023-11-08 18:40:39 -0300

    HOLA

--- carousel.js
+++ carousel.js
@@ -1,15 +1,14 @@
 const slider = document.querySelector('.slider');
 const items = document.querySelectorAll('.item');
-let autoSlideInterval; // Variable para almacenar el temporizador
+let autoSlideInterval;
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
   e.target.matches('.next') && slider.append(items[0]);
   e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
   
-  // Detener el temporizador actual y configurar uno nuevo al hacer clic
   clearInterval(autoSlideInterval); 
-  autoSlideInterval = setInterval(autoSlide, 3000); // Reiniciar el temporizador cada vez que haces clic
+  autoSlideInterval = setInterval(autoSlide, 3000);
 }
 
 function autoSlide() {
@@ -19,6 +18,4 @@ function autoSlide() {
 document.addEventListener('click', activate, false);
 
 
-
-// Configurar el primer temporizador para avanzar automáticamente las imágenes
 autoSlideInterval = setInterval(autoSlide, 3000);
